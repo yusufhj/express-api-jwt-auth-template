5cd37a6f8b4052e9664f89bbb8548d8a500dd084
Yusuf AlHamad <ylive71@gmail.com>
2024-11-12 15:56:16 +0300

    finished server jwt

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 require('./config/database');
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
+const verifyToken = require('./middleware/verify-token');
 
 // Controllers
 const testJwtCtrl = require('./controllers/test-jwt');
 const usersRouter = require('./controllers/users');
+const profilesRouter = require('./controllers/profiles');
 
+const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
@@ -16,6 +18,8 @@ app.use('/test-jwt', testJwtCtrl);
 app.use('/users', usersRouter);
 
 // Private Routes
+app.use(verifyToken);
+app.use('/profiles', profilesRouter);
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
